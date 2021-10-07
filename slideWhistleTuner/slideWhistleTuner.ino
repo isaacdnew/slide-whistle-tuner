1d5702f03c2e4fde42e7e8bcca8201a3dc086674
Isaac Newcomb <isaacdnew@gmail.com>
2021-10-07 16:45:09 -0400

    4:45 pm

--- slideWhistleTuner/slideWhistleTuner.ino
+++ slideWhistleTuner/slideWhistleTuner.ino
@@ -1,7 +1,7 @@
 /*
 
 Original Author: Clyde A. Lettsome, PhD, PE, MEM
-Adapted by Isaac Newcomb
+Adapted significantly by Isaac Newcomb
 
 Description: This code/sketch makes displays the approximate frequency of the loudest sound detected by a sound detection module. For this project, the analog output from the
 sound module detector sends the analog audio signal detected to A0 of the Arduino Uno. The analog signal is sampled and quantized (digitized). A Fast Fourier Transform (FFT) is
@@ -13,36 +13,47 @@ Note: The arduinoFFT.h library needs to be added to the Arduino IDE before compi
 License: This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (GPL) version 3, or any later
 version of your choice, as published by the Free Software Foundation.
 
-Notes: Copyright (c) 2019 by C. A. Lettsome Services, LLC
+Notes: Copyright (c) 2019 by C. A. Lettsome Services, LLC. Revisions Copyright (c) 2021 Isaac Newcomb.
 For more information visit https://clydelettsome.com/blog/2019/12/18/my-weekend-project-audio-frequency-detector-using-an-arduino/
 
 */
 
 #include "arduinoFFT.h"
 
-arduinoFFT FFT = arduinoFFT();
+arduinoFFT FFT;
 
+// analog pins
+const unsigned int MIC_PIN = 0; // the Arduino analog pin for the mic
+const unsigned int POT_PIN = 1; // the Arduino analog pin for the potentiometer
+
+// timekeeping and indexing
+unsigned long sampleBirthday; // the micros() time when the last sample was recorded
+unsigned int i = 0; // index of sample to record
+
+// FFT config
 const unsigned int SAMPLES = 128; // must be a base 2 number; max 128 for Arduino Uno
 const unsigned int SAMPLING_FREQ = 2048; // must be twice the highest expected frequency
 const unsigned int SAMPLING_PERIOD = round(1000000*(1.0/SAMPLING_FREQ)); // sampling period in microseconds
 
-const unsigned int MIC_PIN = 0; // the analog pin on the arduino that's connected to the mic
-
-unsigned long sampleBirthday; // the micros() time when the last sample was recorded
-unsigned int i = 0; // how many sample
-
+// FFT working vars
 double vReal[SAMPLES]; // create vector of size SAMPLES to hold real values
 double vRealTmp[SAMPLES]; // temporary one to build things in
-double vImag[SAMPLES]; // empty; should initialize as 0s
+bool vRealIsNew = false; // indicates when to recalculate the FFT
+double vImag[SAMPLES]; // empty; should initialize as 0s and remain as such
+double peakPitch; // MIDI pitch of highest peak
 
 void setup() {
-	Serial.begin(9600); // TODO remove for speed?
+	// TODO configure Arduino ports
+	
+	Serial.begin(9600); // TODO remove for faster loop speed?
+	
+	sampleBirthday = micros(); // pretend we just took a sample (to start the loop correctly)
 }
 
 void loop() {
 	
-	// Get Samples!
-	if (i < SAMPLES) { // if we've collected at most SAMPLES samples so far (i==127 means we have 128 total samples)
+	// get samples as needed
+	if (i < SAMPLES) { // if we've collected at most SAMPLES samples so far (i==0 means we have 1 sample)
 		
 		// wait for a sampling period to have passed
 		if (micros() - sampleBirthday >= SAMPLING_PERIOD) {
@@ -56,17 +67,38 @@ void loop() {
 	} else {
 		
 		vReal = vRealTmp; // copy from tmp var to main var
+		vRealIsNew = true;
 		i = 0; // go back to recording samples
 		
 	}
 	
-	// do FFT on snippet
-	FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-	FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
-	FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+	if (vRealIsNew) {
+		vRealIsNew = false;
+		// do FFT on snippet
+		// use hamming windowing to get narrower pitches with more frequency bleed
+		// use hann    windowing to get wider pitches and less frequency bleed
+		FFT = arduinoFFT(vReal, vImag, SAMPLES, SAMPLING_FREQ);
+		FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+		FFT.Compute(FFT_FORWARD);
+		FFT.ComplexToMagnitude();
+		
+		// find dominant MIDI pitch (formula from https://newt.phys.unsw.edu.au/jw/notes.html)
+		peakPitch = 12.0 * log2(FFT.MajorPeak() / 440.0) + 69;
+		
+		// // print dominant MIDI pitch
+		// Serial.print("Dominant MIDI Pitch: ");
+		// Serial.println(peakPitch);
+		
+		// find correction amount
+		corr = findCorrection(peakPitch, str)
+		
+		// convert to servo angle
+	}
+	
+	
+}
 
-	// find dominant frequency and print it
-	double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQ);
-	Serial.print("peak frequency: ");
-	Serial.println(peak);
+double findCorrection(double pitchIn, double strength) {
+	// TODO check if pitch is in a reasonable range for better reliability
+	
 }
