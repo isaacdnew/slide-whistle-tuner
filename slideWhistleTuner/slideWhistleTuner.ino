96e0ebf72600bd0aa8d225385d24f257803cc933
Isaac Newcomb <isaacdnew@gmail.com>
2021-10-08 00:16:17 -0400

    12:16 am

--- slideWhistleTuner/slideWhistleTuner.ino
+++ slideWhistleTuner/slideWhistleTuner.ino
@@ -21,18 +21,20 @@ For more information visit https://clydelettsome.com/blog/2019/12/18/my-weekend-
 #include "arduinoFFT.h"
 #include <Servo.h>
 
-arduinoFFT FFT;
+// Arduino pins
+const int MIC_PIN = 0; // analog input
+const int POT_PIN = 1; // analog input
+const int SERVO_PIN = 2; // PWM output
+const int LED_UP = 3; // digital output
+const int LED_GOOD = 4; // digital output
+const int LED_DOWN = 5; // digital output
 
-// analog pins
-const int MIC_PIN = 0;
-const int POT_PIN = 1;
-const int SERVO_PIN = 2;
 
-// timekeeping and indexing
-long sampleBirthday; // the micros() time when the last sample was recorded
-int i = 0; // index of sample to record
+Servo servo;
+const double SERVO_ARM_LG = 15.0; // mm
 
 // FFT config
+arduinoFFT fft;
 const int SAMPLES = 128; // must be a base 2 number; max 128 for Arduino Uno
 const int SAMPLING_FREQ = 2048; // must be twice the highest expected frequency
 const int SAMPLING_PERIOD = round(1000000*(1.0/SAMPLING_FREQ)); // sampling period in microseconds
@@ -41,6 +43,8 @@ const int SAMPLING_PERIOD = round(1000000*(1.0/SAMPLING_FREQ)); // sampling peri
 double vReal[SAMPLES]; // create vector of size SAMPLES to hold real values
 double vImag[SAMPLES]; // empty; should initialize as 0s and remain as such
 bool snippetIsReady = false; // indicates when to recalculate the FFT
+long sampleBirthday; // the micros() time when the last sample was recorded
+int i = 0; // index of sample to record
 
 // pitches to correct to - select by (un)commenting
 // C Major
@@ -53,7 +57,7 @@ const int PITCH_SET[PITCH_SET_LG] =
 	84, 86, 88, 89, 91, 93, 95, 96
 };
 
-// C Minor
+// // C Minor
 // const int PITCH_SET_LG = 29;
 // const int PITCH_SET[PITCH_SET_LG] =
 // {
@@ -63,7 +67,7 @@ const int PITCH_SET[PITCH_SET_LG] =
 // 	84, 86, 87, 89, 91, 92, 94, 96
 // };
 
-// Chromatic
+// // Chromatic
 // const int PITCH_SET_LG = 49;
 // const int PITCH_SET[PITCH_SET_LG] =
 // {
@@ -73,14 +77,23 @@ const int PITCH_SET[PITCH_SET_LG] =
 // 	84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96
 // };
 
+// physical/musical constants
+const double SOUND_SPEED = 343.2; // m/s
+const double A4_FREQ = 440.0; // Hz
+
 void setup()
 {
-	// TODO configure Arduino ports
+	// configure Arduino ports
 	pinMode(MIC_PIN, INPUT);
 	pinMode(POT_PIN, INPUT);
+	servo.attach(SERVO_PIN);
+	pinMode(LED_UP, OUTPUT);
+	pinMode(LED_GOOD, OUTPUT);
+	pinMode(LED_DOWN, OUTPUT);
 	
 	Serial.begin(9600); // TODO remove for faster loop speed?
 	
+	fft = arduinoFFT();
 	sampleBirthday = micros(); // pretend we just took a sample (to start the loop correctly)
 }
 
@@ -105,24 +118,27 @@ void loop()
 		// do FFT on snippet of samples
 		// use hamming windowing to get narrower pitches with more frequency bleed
 		// use hann    windowing to get wider pitches and less frequency bleed
-		FFT = arduinoFFT(vReal, vImag, SAMPLES, SAMPLING_FREQ);
-		FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-		FFT.Compute(FFT_FORWARD);
-		FFT.ComplexToMagnitude();
+		fft.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+		fft.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+		fft.ComplexToMagnitude(vReal, vImag, SAMPLES);
 		
 		// find dominant MIDI pitch (formula from https://newt.phys.unsw.edu.au/jw/notes.html)
-		double peakPitch = 12.0 * log(FFT.MajorPeak()/440.0) / log(2.0) + 69;
+		double peakFreq = fft.MajorPeak(vReal, SAMPLES, SAMPLING_FREQ);
+		double peakPitch = freq2Pitch(peakFreq);
 		
-		// find correction amount
-		double corr = findCorrection(peakPitch, analogRead(POT_PIN));
+		// find pitch correction amount and corrected freqency
+		double corr = correction(peakPitch, analogRead(POT_PIN));
+		double correctedFreq = pitch2Freq(pitchIn + corr);
 		
-		// convert to servo angle
+		updateServo(correctedFreq, peakFreq);
 		
+		updateLEDs(corr, 0.10); // tolerance units: MIDI pitch
 	}
-	
 }
 
-double findCorrection(double pitchIn, double strength)
+// find the (signed) amount of pitch correction to apply,
+// given an input pitch and a strength from 0 to 1
+double correction(double pitchIn, double strength)
 {
 	// TODO check if pitch is in a reasonable range for better reliability
 	int idx = 0;
@@ -140,5 +156,68 @@ double findCorrection(double pitchIn, double strength)
 		}
 	}
 	
-	return strength * (pitchIn - PITCH_SET[idx]);
+	return strength * (PITCH_SET[idx] - pitchIn);
+}
+
+// update the LEDs based on amount of correction applied
+void updateLEDs(double corr, double tol)
+{
+	// choose which LED to light
+	if (corr > tol)
+	{
+		// light the LED for high
+		digitalWrite(LED_UP, HIGH);
+		digitalWrite(LED_GOOD, LOW);
+		digitalWrite(LED_DOWN, LOW);
+	}
+	else if (corr < -tol)
+	{
+		// light the LED for down
+		digitalWrite(LED_UP, LOW);
+		digitalWrite(LED_GOOD, LOW);
+		digitalWrite(LED_DOWN, HIGH);
+	}
+	else
+	{
+		// light the LED for good
+		digitalWrite(LED_UP, LOW);
+		digitalWrite(LED_GOOD, HIGH);
+		digitalWrite(LED_DOWN, LOW);
+	}
+}
+
+// move the servo to the angle that yields a perfect correction;
+// otherwise rotate it to center.
+void updateServo(double correctedFreq, double realFreq)
+{
+	// convert to servo angle and move servo
+	double realLength = ((SOUND_SPEED / peakFreq) * 1000.0) / 4.0; // mm
+	double desiredLength = ((SOUND_SPEED / correctedFreq) * 1000.0) / 4.0; // mm
+	double corrDist = desiredLength - realLength; // mm
+	
+	// servo angle is 0-180, so 90 is centered
+	if (corrDist > SERVO_ARM_LG)
+	{
+		// can't make tube long enough; make it as long as possible
+		servo.write(180);
+	}
+	else if (corrDist < -SERVO_ARM_LG)
+	{
+		// can't make tube short enough; make it as short as possible
+		servo.write(0);
+	}
+	else
+	{
+		servo.write(90 + asin(corrDist / SERVO_ARM_LG));
+	}
+}
+
+double freq2Pitch(double freq)
+{
+	return 12.0 * log(freq/A4_FREQ) / log(2) + 69;
+}
+
+double pitch2Freq(double pitch)
+{
+	return A4_FREQ * 2^((pitch - 69) / 12.0);
 }
